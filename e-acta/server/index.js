191cdaeadef34c89411e949ecca401767e54c219
Javier MR <jmoro.ribera@gmail.com>
2021-03-24 19:43:37 +0100

    Added comment

--- e-acta/server/index.js
+++ e-acta/server/index.js
@@ -9,3 +9,12 @@ app.listen(3001, ()=>{
     console.log("running on port 3001");
 })
 
+/*
+
+Bodyparser is currently deprecated in Express 4 (NodeJS), urlencoded is already in the new version of express, t
+the way to use urlencoded is using this line: app.use (express.urlencoded ({extended: false})); 
+// false if you only want to pass flat text
+
+*/
+
+
