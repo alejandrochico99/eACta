567d7e3caa2ca5f58472c53802ced2741dd11499
alejandrochico99 <soporte@oonlive.com>
2021-04-07 10:46:46 +0200

    Prueba Andres

--- e-acta/client/src/App.js
+++ e-acta/client/src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <Router>
-      <Switch>
+      <Switch> 
         <Route exact path="/" component={General} />
         <Route path="/asignaturas" component={Asignaturas} />
       </Switch>
