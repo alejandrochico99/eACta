d734e707563b0bb10f49f7f0969adc65e87c4e86
Javier MR <jmoro.ribera@gmail.com>
2021-04-17 20:27:02 +0200

    arreglado el problema

--- e-acta/client/src/Login.js
+++ e-acta/client/src/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import "./css/Login.css";
+import "./css/login.css";
 import {Link} from "react-router-dom";
 
 export default function Login() {
